d1c1a4da40865dd3f441e524644b85d1954b3494
Pradip Sable <sablepradipsubhash@gmail.com>
2024-02-09 17:05:49 +0530

    initial commit - added all items and like add, read, update, delete in to the node app

---
+++ models/productModels.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "please enter product name.."],
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		quantity: {
+			type: Number,
+			required: true,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		images: { type: String, required: false },
+	},
+	{
+		timestamps: true,
+	}
+);
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
